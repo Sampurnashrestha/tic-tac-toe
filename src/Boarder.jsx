64533b4d2e5a60be0224d2876d96c300cffb14af
Sampurnashrestha <shresthasampurna849@gmail.com>
2026-01-11 20:23:32 +0545

    full finish

--- src/Boarder.jsx
+++ src/Boarder.jsx
@@ -2,26 +2,70 @@ import React, { useState } from 'react'
 import Box from './Box'
 
 const Boarder = () => {
-    const box=[1,2,3,4,5,6,7,8,9];
-    const [boxtext, setBoxText] = useState([null,null,null,null,null,null,null,null,null])
+  const box = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [boxtext, setBoxText] = useState(Array(9).fill(null))
+  const [isXnext, setISXnext] = useState(true)
+  const calculateWinner = () => {
+    const winnerCondition = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 5],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let index = 0; index < winnerCondition.length; index++) {
+      const [a, b, c] = winnerCondition[index];
+      if (boxtext[a] && boxtext[a] === boxtext[b] && boxtext[a] === boxtext[c]) {
+        return boxtext[a];
+      }
 
-    const handleClick = (boxIndex)=>{
-      console.log("object")
-        setBoxText(prev =>{
-            const newarr =[...prev]
-            newarr[boxIndex] = "x"
-            return newarr
-        })
     }
+    return null
+  }
+  const winner = calculateWinner(boxtext)
+  const handleClick = (boxIndex) => {
+
+    setBoxText(prev => {
+      if (prev[boxIndex] || winner) return prev
+      const newarr = [...prev]
+      newarr[boxIndex] = isXnext ? "X" : "O";
+      setISXnext(!isXnext)
+      return newarr
+    })
+  }
+
+  const reset = () => {
+    if (!winner) return
+    setBoxText(Array(9).fill(null))
+    setISXnext(true)
+  }
 
   return (
     <>
+      <div className=' flex flex-col '>
+        <h2 className="text-lg font-semibold">
+          {winner ? `Winner: ${winner}` : ``}
+        </h2>
+        {isXnext ? "PLayer: 1" : "Player 2"}
         <div className='grid grid-cols-3'>
-        {box.map((boxs,i)=>
-                <Box key={boxs} onClicks={()=>handleClick(i)} text={boxtext[i]}/>
-            )}
-            </div>
-      
+          {box.map((boxs, i) =>
+            <Box key={boxs} onClicks={() => handleClick(i)} text={boxtext[i]} />
+          )}
+        </div>
+     
+      <button
+        onClick={reset}
+        disabled = {!winner}
+        className={`mt-4 px-2 py-2 bg-red-500  text-white  rounded  ${winner ? "  hover:bg-red-600 cursor-pointer" : "hover:bg-red-700  cursor-not-allowed"}`}
+       >
+        Reset
+       </button>
+   
+       
+         </div>
     </>
   )
 }
